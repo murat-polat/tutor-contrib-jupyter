0b2cad91f07c6e36c1a31aaf7d29ce0890424c97
Murat Polat <muratsdepo@gmail.com>
2021-06-26 21:06:11 +0200

    Tornado settings changed

--- tutorjupyter/templates/jupyter/build/jupyter/jupyterhub_config.py
+++ tutorjupyter/templates/jupyter/build/jupyter/jupyterhub_config.py
@@ -1,2 +1,5 @@
 
 c.LocalAuthenticator.create_system_users = True
+c.Spawner.args = ['--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors * jupyter:8000"}}']   
+
+c.JupyterHub.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors * jupyter:8000"} }
