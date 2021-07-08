c957673a40ed4f66b6c2c67794b91213eb7e50ff
Murat Polat <muratsdepo@gmail.com>
2021-07-08 13:16:22 +0200

    added admin users and control panel

--- tutorjupyter/templates/jupyter/build/jupyter/jupyterhub_config.py
+++ tutorjupyter/templates/jupyter/build/jupyter/jupyterhub_config.py
@@ -1,5 +1,11 @@
 
-c.LocalAuthenticator.create_system_users = True
 c.Spawner.args = ['--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors * jupyter:8000"}}']   
 
 c.JupyterHub.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors * jupyter:8000"} }
+
+## Users
+c.Authenticator.whitelist = {'student', 'teacher'}
+## Admins
+c.Authenticator.admin_users = {'teacher'}
+## Admins can create/delete users
+c.LocalAuthenticator.create_system_users=True
